3a7aafcf3aaf3f4551a6b4732b4d7b28f440e895
Anton Petrov <beavean@gmail.com>
2023-04-26 19:36:01 +0300

    feat: Add custom font extension to UIFont

---
+++ PokeGuide/Extensions/UIFont+CustomFonts.swift
@@ -0,0 +1,28 @@
+//
+//  UIFont+CustomFonts.swift
+//  PokeGuide
+//
+//  Created by Beavean on 25.04.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static func regularTextCustomFont(size: CGFloat = 14) -> UIFont? {
+        if let font = UIFont(name: Constants.Fonts.latoRegular, size: size) {
+            return font
+        } else {
+            print("\(#function) not found")
+            return nil
+        }
+    }
+
+    static func boldTextCustomFont(size: CGFloat = 18) -> UIFont? {
+        if let font = UIFont(name: Constants.Fonts.latoBold, size: size) {
+            return font
+        } else {
+            print("\(#function) not found")
+            return nil
+        }
+    }
+}
