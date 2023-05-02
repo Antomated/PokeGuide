12668e9dcd05ea2a6f92ee40e2b3245fa5d2403e
Anton Petrov <beavean@gmail.com>
2023-05-02 23:42:06 +0300

    feat: Add setupNavigationController function with root view controller and title configuration

--- PokeGuide/AppDelegate.swift
+++ PokeGuide/AppDelegate.swift
@@ -4,18 +4,36 @@
 //
 //  Created by Beavean on 24.04.2023.
 //
+// TODO: remove prints
+// TODO: If to guard
 
+import SDWebImage
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = setupNavigationController()
+        SDWebImageDownloader.shared.config.maxConcurrentDownloads = 5
         return true
+    }
+
+    private func setupNavigationController() -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: PokemonsViewController())
+        navigationController.navigationBar.prefersLargeTitles = true
+        guard let font = UIFont.boldTextCustomFont(size: 24) else { return navigationController }
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: Constants.Colors.mainAccentColor.color as Any
+        ]
+        navigationController.navigationBar.largeTitleTextAttributes = attributes
+        navigationController.navigationBar.titleTextAttributes = attributes
+
+        return navigationController
     }
 }
