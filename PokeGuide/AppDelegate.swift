04b96b37f404ad75de2d7afee17aedffa3868116
Anton Petrov <beavean@gmail.com>
2023-05-11 14:34:25 +0300

    feat: Implement customizedNavigationController as computed property

--- PokeGuide/AppDelegate.swift
+++ PokeGuide/AppDelegate.swift
@@ -11,21 +11,20 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    private var customizedNavigationController: UINavigationController {
+        let navigationController = UINavigationController(rootViewController: PokemonsViewController())
+        navigationController.interactivePopGestureRecognizer?.isEnabled = true
+        navigationController.interactivePopGestureRecognizer?.delegate = nil
+        navigationController.navigationBar.prefersLargeTitles = true
+        return navigationController
+    }
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         SDWebImageDownloader.shared.config.maxConcurrentDownloads = 5
         window?.makeKeyAndVisible()
-        window?.rootViewController = setupNavigationController()
+        window?.rootViewController = customizedNavigationController
         return true
-    }
-
-    private func setupNavigationController() -> UINavigationController {
-        let navigationController = UINavigationController(rootViewController: PokemonsViewController())
-        navigationController.interactivePopGestureRecognizer?.isEnabled = true
-        navigationController.interactivePopGestureRecognizer?.delegate = nil
-        navigationController.navigationBar.prefersLargeTitles = true
-        return navigationController
     }
 }
