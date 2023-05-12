98bf0f264ac2cce4fa60afa4ffc84976156959f3
Anton Petrov <beavean@gmail.com>
2023-05-12 15:06:47 +0300

    feat: Add MockPokemonAPIManager to Unit tests

---
+++ PokeGuideTests/Mocks/MockPokemonAPIManager.swift
@@ -0,0 +1,28 @@
+//
+//  MockPokemonAPIManager.swift
+//  PokeGuideTests
+//
+//  Created by Beavean on 11.05.2023.
+//
+
+import XCTest
+import RxSwift
+@testable import PokeGuide
+
+final class MockPokemonAPIManager: PokemonAPIManager {
+    var shouldReturnError = false
+
+    override func fetchData<T>(from endpoint: PokemonAPI, ofType type: T.Type) -> Observable<T> where T: Decodable {
+        if shouldReturnError {
+            return Observable.error(NSError(domain: "", code: -1, userInfo: nil))
+        }
+        if type == PokemonList.self {
+            let pokemonList = PokemonList(next: nil, results: [BasicPokemon(name: "test", url: "test")])
+            guard let castedPokemonList = pokemonList as? T else {
+                return Observable.empty()
+            }
+            return Observable.just(castedPokemonList)
+        }
+        return Observable.empty()
+    }
+}
